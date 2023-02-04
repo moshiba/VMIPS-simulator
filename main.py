2bae9f3c58c8a5ee87524b822fa255f83f5f1bf3
Hsuan-Ting Lu <hsuan.ting.lu@nyu.edu>
2023-02-04 18:57:53 -0500

    Add instruction memory class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pathlib
 import abc
+import io
+import copy
 
 
 class FileMap(abc.ABC):
@@ -29,3 +31,34 @@ class FileMap(abc.ABC):
     @abc.abstractmethod
     def data(self, file) -> None:
         raise NotImplementedError
+
+
+class InstructionMemory(FileMap):
+
+    def __init__(self, /, load_path: str):
+        super().__init__(load_path=load_path, dump_path="")
+        self.size_limit = pow(2, 16)
+        self.instructions = tuple()
+
+    def __getitem__(self, index) -> str:
+        if index > self.size_limit:
+            raise IndexError(f"Invalid memory access at index {index}"
+                             f" with memory size {self.size_limit}")
+        return self.instructions[index]
+
+    def dump(self):
+        raise NotImplementedError
+
+    @property
+    def data(self) -> str:
+        instructions = copy.copy(self.instructions)
+        # Strip trailing empty instructions
+        while not instructions[-1]:
+            instructions.pop()
+        return tuple(instructions)
+
+    @data.setter
+    def data(self, file: io.TextIOWrapper):
+        instructions = [instr.strip() for instr in file.readlines()]
+        assert len(instructions) < self.size_limit, "too many instructions"
+        self.instructions = instructions
