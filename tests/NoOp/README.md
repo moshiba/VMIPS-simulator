e038d28a0fe97b3cac3e1386bdcdb511f0219979
Hsuan-Ting Lu <hsuan.ting.lu@nyu.edu>
2023-02-05 01:38:59 -0500

    Add tests for the register file class
    Add test cases for load/dump format and their golden results.

---
+++ tests/NoOp/README.md
@@ -0,0 +1,5 @@
+# Golden results for the No-Op (do nothing) test case
+
+1. Load ```SDMEM.txt``` and ```VDMEM.txt```.
+2. Run no code, do nothing.
+3. An immediate dump should return results exactly matching ```SRF.txt```, ```VRF.txt```, ```SDMEMOP.txt```, and ```VDMEMOP.txt```.
---
+++ tests/test_components.py
@@ -0,0 +1,51 @@
+"""Test component functionality like operations and load/dump format
+"""
+import pathlib
+import tempfile
+import unittest
+
+import main
+
+GoldenFiles = pathlib.Path("tests/NoOp")
+assert GoldenFiles.is_dir()
+
+
+class BaseTestWithTempDir(unittest.TestCase):
+    """Base class that creates a temporary directory for tests to operate on
+    """
+
+    def setUp(self) -> None:
+        self._temp_dir = tempfile.TemporaryDirectory()
+        self.temp_dir = pathlib.Path(self._temp_dir.name)
+
+    def tearDown(self) -> None:
+        self._temp_dir.cleanup()
+
+
+class TestRegisterFile(BaseTestWithTempDir):
+
+    def test_dump_scalar_regfile(self):
+        golden_file = GoldenFiles / "SRF.txt"
+        result_file = self.temp_dir / "SRF.txt"
+
+        regfile = main.RegisterFile(dump_path=result_file,
+                                    n_reg=8,
+                                    vec_size=1,
+                                    word_size=32)
+        regfile.dump()
+
+        with golden_file.open() as golden, result_file.open() as result:
+            self.assertEqual(golden.read(), result.read())
+
+    def test_dump_vector_regfile(self):
+        golden_file = GoldenFiles / "VRF.txt"
+        result_file = self.temp_dir / "VRF.txt"
+
+        regfile = main.RegisterFile(dump_path=result_file,
+                                    n_reg=8,
+                                    vec_size=64,
+                                    word_size=32)
+        regfile.dump()
+
+        with golden_file.open() as golden, result_file.open() as result:
+            self.assertEqual(golden.read(), result.read())
